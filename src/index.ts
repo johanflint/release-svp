3ccf98bc17fa50c07b6a52847d68b3068e2b8594
Johan Flint <johan.flint@gmail.com>
2025-11-10 20:12:18 +0100

    Add command to prepare releases
    Introduces a 'prepare' command using yargs to handle command-line arguments.
    This command parses a GitHub repository URL and prints information about the repository to prepare for a release.
    
    The command validates the repository URL and extracts the owner and repository name.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,53 @@
-console.log("Release SVP!");
+import * as yargs from "yargs";
+import { Repository } from "./repository";
+
+interface GitHubArgs {
+    token?: string;
+    repoUrl?: string;
+}
+
+function gitHubOptions(yargs: yargs.Argv<GitHubArgs>): yargs.Argv {
+    return yargs
+        .option("token", {
+            describe: "GitHub token with repository write permissions",
+        })
+        .option("repo-url", {
+            describe: "GitHub URL to generate a release for",
+            demandOption: true,
+            type: "string",
+        })
+}
+
+const prepareCommand: yargs.CommandModule<{}, GitHubArgs> = {
+    builder(yargs) {
+        return gitHubOptions(yargs);
+    },
+    async handler(args: yargs.ArgumentsCamelCase<GitHubArgs>) {
+        const repository = parseGitHubUrl(args.repoUrl ?? "");
+        if (!repository.owner || !repository.repo) {
+            console.error(`Invalid GitHub repository url '${args.repoUrl}, expected repository/owner format'`);
+            return;
+        }
+
+        console.info(`Prepare release for repository '${repository.owner}/${repository.repo}'`);
+    },
+    command: "prepare",
+    describe: "Create or update a pull request representing the next release"
+};
+
+function parseGitHubUrl(url: string): Repository {
+    const match = /^([\w-.]+)\/([\w-.]+)$/.exec(url)
+    return {
+        owner: match?.[1] ?? "",
+        repo: match?.[2]?? "",
+    }
+}
+
+const parser = yargs.command(prepareCommand)
+    .demandCommand(1)
+    .strict(true)
+    .scriptName("release-svp");
+
+(async () => {
+    await parser.parseAsync();
+})();
